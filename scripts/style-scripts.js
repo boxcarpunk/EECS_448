f1208f14825ec2b570532c120f065762a1d8e7b6
David Iordan <davidiordan@icloud.com>
2018-02-04 21:44:29 -0600

    Cleaned up file format, added links to documentation file, font chosen

--- scripts/style-scripts.js
+++ scripts/style-scripts.js
@@ -1,17 +1,15 @@
-/* Side Navigation animation from: https://www.w3schools.com/howto/howto_js_sidenav.asp */
+/* Side Navigation, source in documentaion.txt under "Side Navigation" */
 
 function openNav() {
-    $("#sideNav").width("250px");
-    document.getElementById("main").style.marginLeft = "250px";
-//    $("#main").style.marginLeft = "250";
-    $("#openBtn").fadeOut();
-    $("#openBtn-wht").fadeOut();
+  $("#sideNav").width("250px");
+//    document.getElementById("main").style.marginLeft = "250px"; //moves page over to fit with sideNav, not working correctly...
+  $("#openBtn").fadeOut();
+  $("#openBtn-wht").fadeOut();
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-    $("#sideNav").width("0");
-    document.getElementById("main").style.marginLeft = "0";
-    $("#openBtn").fadeIn();
-    $("#openBtn-wht").fadeIn();
+  $("#sideNav").width("0");
+//    document.getElementById("main").style.marginLeft = "0"; //moves page over to fit with sideNav, not working correctly...
+  $("#openBtn").fadeIn();
+  $("#openBtn-wht").fadeIn();
 }
