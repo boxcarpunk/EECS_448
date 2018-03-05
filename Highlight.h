85b0bc68b2a0279264b6ca4ac506ab6f5becfcb8
Andrew Hubbard <andrew.hubbard@ku.edu>
2018-03-04 22:44:01 -0600

    Merged Nick with Master
    Master should now be all up to date in terms of both front and back end code

--- Highlight.h
+++ Highlight.h
@@ -4,12 +4,11 @@
 #include <vector>
 class Highlight
 {
-	public:
-		Highlight();
-		int print_menu(WINDOW * win, char * Menu[], int size, int StartLine);
-		int print_scroll(WINDOW * win, char * Menu[], int size, int Actual, int StartLine);
-		int print_vec(WINDOW * win, std::vector<char *> Menu, int Actual, int StartLine);
-	private:
+public:
+	Highlight();
+	int print_menu(WINDOW * win, char * Menu[], int size, int StartLine);
+	int print_scroll(WINDOW * win, char * Menu[], int size, int Actual, int StartLine);
+	int print_vec(WINDOW * win, std::vector<char *> Menu, int Actual, int StartLine);
+private:
 };
 #endif
-
